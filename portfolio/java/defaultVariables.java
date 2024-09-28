6780a060d7ba36799b4a6ac1c1deb1a5fc73c03f
gidwanisahil <81971180+gidwanisahil@users.noreply.github.com>
2024-09-28 20:35:49 +0000

    created some java examples files

---
+++ portfolio/java/defaultVariables.java
@@ -0,0 +1,16 @@
+public class defaultVariables {
+
+    public static void main(String[] args) {
+        String name = "Sahil";
+        System.out.println(name);
+        int myNum = 25;
+        System.out.println(myNum);
+
+        char myLetter ='S' ;
+        System.out.println(myLetter);
+        float myFloatNum = 5.99f;
+        System.out.println(myFloatNum);
+        boolean myBool = true;
+        System.out.println(myBool);
+    }
+}
