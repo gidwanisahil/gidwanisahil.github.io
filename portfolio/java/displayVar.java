13449c5ed21c9c24fb744303c34256af2bb069ae
gidwanisahil <81971180+gidwanisahil@users.noreply.github.com>
2024-09-28 20:45:56 +0000

    added more java files

---
+++ portfolio/java/displayVar.java
@@ -0,0 +1,17 @@
+public class displayVar {
+
+    public static void main(String[] args) {
+        String name = "Sahil ";
+        System.out.println("Hello "+ name);
+
+        String firstName="Sahil ";
+        String lastName="Gidwani";
+        String fullName=firstName+lastName;
+        System.out.println(fullName);
+        int x = 5;
+int y = 6;
+System.out.println(x + y); // Print the value of x + y
+
+
+    }
+}
